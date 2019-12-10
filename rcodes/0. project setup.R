b3480e51b564e71268ece81bf74b6a0bfc02d975
Luo <Yong.Luo@gov.bc.ca>
2019-12-10 14:28:26 -0800

    [setup project] created

---
+++ rcodes/0. project setup.R
@@ -0,0 +1,11 @@
+## run for the first time, and require data from Wenli, Yong or Yingbing
+## for setup folders
+
+
+dir.create(file.path(".", "rcodes"))
+dir.create(file.path(".", "data"))
+dir.create(file.path(".", "data", "rawdata"))
+dir.create(file.path(".", "data", "compileddata"))
+dir.create(file.path(".", "results"))
+dir.create(file.path(".", "documents"))
+dir.create(file.path(".", "reference"))
