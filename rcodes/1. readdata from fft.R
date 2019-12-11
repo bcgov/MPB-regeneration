c2fbc226f86a6afe37b450510be8ea384fff6d53
Chen <Yingbing.Chen@gov.bc.ca>
2019-12-11 11:16:57 -0800

    [read fft data]function for extracing BAF count table created

--- rcodes/1. readdata from fft.R
+++ rcodes/1. readdata from fft.R
@@ -91,41 +91,81 @@ CreaHATable<-function(fftfile){
   return(hatable)
 }
 
+####Create BAF count table#########################
+
+CreaBafTable<-function(fftfile){
+  xlsxfile<-file.path(fftdatapath, fftfile)
+  sumtable<-read.xlsx(xlsxfile)
+  baftable<-NULL
+  for (i in 1:sumtable[9,5]){
+    tmp <- read.xlsx(xlsxfile,
+                     sheet=as.character(i),
+                     colNames = FALSE,
+                     detectDates = TRUE)
+    test<-tmp[19:20,1:8]
+    names(test)<-as.character(test[1,])
+    names(test)[1]<-"BAF"
+    test<-test[-1,]
+    test[is.na(test$BAF),"BAF"]<-"5"
+    test<-test[,!apply(is.na(test),2,all)]
+    if (is.data.frame(test)){
+      test1<-reshape(test,
+                     varying = 2:dim(test)[2],
+                     v.names = "count",
+                     times=names(test)[2:dim(test)[2]],
+                     direction="long") #reshape to long table##
+      test2<-separate(test1,
+                      col = "time",
+                      into = c("SPP","Layer"),
+                      sep = " ",
+                      fill = "left")
+      test2<-subset(test2,select = -id)
+      row.names(test2)<-NULL
+      openingid<-tmp[1,3] #add opening id
+      plotid<-tmp[1,9] #add plot id
+      date<-tmp[1,11] #add measure date
+      test3<-cbind(openingid,date,plotid,test2)
+    }else{
+      test3<-NULL
+    }
+    baftable<-rbind(baftable,test3)
+  }
+  row.names(baftable)<-NULL
+  return(baftable)
+}
+
+
 
 ######TEST RUN for one opening and one plot#######
 FFT_93G_045_568<-read.xlsx(file.path(fftdatapath, "93G_045_568_MPB_Recce_Survey_Strat2.xlsx"))
 for (i in 1:FFT_93G_045_568[9,5]){
-  i<-1
-  tmp <- read.xlsx(FFT_93G_045_568<-read.xlsx(file.path(fftdatapath, "93G_045_568_MPB_Recce_Survey_Strat2.xlsx")),
+  i<-6
+  tmp <- read.xlsx(file.path(fftdatapath, "93G_045_568_MPB_Recce_Survey_Strat2.xlsx"),
                    sheet=as.character(i),
                    colNames = FALSE,
                    detectDates = TRUE)
 }
-test<-tmp[12:18,1:8]
+test<-tmp[19:20,1:8]
 names(test)<-as.character(test[1,])
+names(test)[1]<-"BAF"
 test<-test[-1,]
+test[is.na(test$BAF),"BAF"]<-"5"
 test<-test[,!apply(is.na(test),2,all)] #remove na columns
-names(test)[1]<-"Attributes" #replace NA to "Attributes" for first column's name
-test1<-separate(test,col = "Attributes", into = c("Layer","Attribute"),sep = " ") #seperate layer and attributes
-test1<-reshape(test1,varying = 3:dim(test1)[2],v.names = "number",times=names(test1)[3:dim(test1)[2]],direction="long") #reshape to long table##
-sub_Ht<-test1[test1$Attribute=="Ht",]
-sub_Age<-test1[test1$Attribute=="Age",]
-test2<-merge(sub_Ht,sub_Age,by = c("Layer","time"),suffixes = c("Ht","Age"))
-test3<-subset(test2,select = c(Layer,time,numberHt,numberAge))
-names(test3)<-c("Layer","SPP","Ht","Age")
-test4<-test3[!apply(is.na(test3),1,any),]
+test1<-reshape(test,varying = 2:dim(test)[2],v.names = "count",times=names(test)[2:dim(test)[2]],direction="long") #reshape to long table##
+test2<-separate(test1,
+                col = "time",
+                into = c("SPP","Layer"),
+                sep = " ",
+                fill = "left")
+test2<-subset(test2,select = -id)
+row.names(test2)<-NULL
 openingid<-tmp[1,3] #add opening id
 plotid<-tmp[1,9] #add plot id
 date<-tmp[1,11] #add measure date
-test5<-cbind(openingid,date,plotid,test4)
+test3<-cbind(openingid,date,plotid,test2)
 
 ###save for later memo#####
-FFT_93J_073_077<-read.xlsx("C:/data/FFTmpb/93J_073_077_2018_MPB_Recce_Summary.xlsx")
-htagetable<-rbind(htagetable, tmp[12:18,1:8])
-baftable<-rbind(baftable,)
-lapply()
-a<-paste("FFT_93G_045_568",c(1:FFT_93G_045_568[9,5]),sep="_")
-names(test)<-a
-list2env(test,envir=.GlobalEnv)
+FFT_93J_073_077<-read.xlsx(file.path(fftdatapath, "93J_073_077_2018_MPB_Recce_Summary.xlsx"))
+
 
 
