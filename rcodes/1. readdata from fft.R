956274d2ff12f5717f162981781f0ec645085c1d
Chen <Yingbing.Chen@gov.bc.ca>
2019-12-12 11:08:22 -0800

    [read fft data]revision

--- rcodes/1. readdata from fft.R
+++ rcodes/1. readdata from fft.R
@@ -5,23 +5,38 @@ rm(list=ls())
 library(openxlsx)
 library(tidyr)
 fftdatapath <- file.path(".", "data", "rawdata", "fft")
+file_list <- dir(fftdatapath, full.names = TRUE)
 
 ####Summarise tables from all avaliable fft files########
 ####Extract and rashape tables from FFT survey data: 1. count table; 2. Ht-Age table; 3. BAF count table; 4. Forest health table
-####1.count table
-file_list <- dir(fftdatapath, full.names = TRUE)
 
-counTable_All <- NULL
+Opening_Info <- NULL
+CounTable_All <- NULL
 HtAgeTable_All <- NULL
 BafTable_All <- NULL
 HealTable_All <- NULL
-for (indifile in file_list){
-  indifile <- file_list[1]
+for (i in 1:length(file_list)){
+  indifile <- file_list[i]
   reportTable <- read.xlsx(indifile,
-                           sheet = "Report") #extract the summary table of this opening
+                           sheet = "Report",
+                           detectDates = TRUE) #extract the summary table of this opening
+  opening_tmp<-as.data.frame(cbind(Opening = reportTable[1,5],
+                                   Openingid = reportTable[2,5],
+                                  Date = reportTable[1,10],
+                                  Lat = reportTable[3,16],
+                                  Long = reportTable[4,16],
+                                  Plot_size_m2 = reportTable[5,16],
+                                  Area_ha = reportTable[8,5],
+                                  plot_Number = reportTable[9,5],
+                                  BEC = reportTable[8,10],
+                                  Mortality = reportTable[57,3]))
+  opening_tmp$Area_ha <- as.numeric(gsub(" ha", "", opening_tmp$Area_ha))
+  opening_tmp$Plot_size_m2 <- as.numeric(gsub("m2","",opening_tmp$Plot_size_m2))
+  opening_tmp$plot_Number <- as.numeric(as.character(opening_tmp$plot_Number))
+  opening_tmp$Mortality <- round(as.numeric(as.character(opening_tmp$Mortality)),digits = 2)
+  Opening_Info<- rbind(Opening_Info,opening_tmp)
 
   NoPlot <- reportTable[9, 5]
-
   for (indiplot in 1:NoPlot) {
     indiplotdata <- read.xlsx(indifile,
                               sheet = as.character(indiplot),
@@ -30,7 +45,7 @@ for (indifile in file_list){
 
     ## extract count table
     tmp_countdata <- CreaCounTable(indiplotdata)
-    counTable_All <- rbind(counTable_All,tmp_countdata)
+    CounTable_All <- rbind(CounTable_All,tmp_countdata)
 
     ####2. Ht-Age table
     tmp_htagedata <- CreaHATable(indiplotdata)
@@ -41,33 +56,31 @@ for (indifile in file_list){
     BafTable_All <- rbind(BafTable_All, tmp_bafdata)
 
     ####4. Forest health table
-    tmp_helthdata <- CreaCounTable(indiplotdata)
+    tmp_helthdata <- CreaHealTable(indiplotdata)
     HealTable_All <- rbind(HealTable_All, tmp_helthdata)
 
-    rm(tmp_countdata, tmp_helthdata, tmp_bafdata, tmp_htagedata)
   }
-
+  rm(tmp_countdata, tmp_helthdata, tmp_bafdata, tmp_htagedata, opening_tmp)
 }
-
 
 
 ####FUNCTIONS################################################
 ####1. FUNCTION for creating count table#####################
 
-CreaCounTable <- function(plotdata_raw){
-  plotdata_raw <- indiplotdata
-  test <- plotdata_raw[2:11,1:8] #extract the count table from the measure table
+CreaCounTable <- function(indiplotdata){
+
+  test <- indiplotdata[2:11,1:8] #extract the count table from the measure table
   names(test) <- as.character(test[1,]) #set the column names to be the first row of count table
   test <- test[-1,] #remove first row as it is now the column names
   test <- test[,!apply(is.na(test),2,all)] #remove na columns
   test1 <- reshape(test,
                  varying = 2:dim(test)[2],
-                 v.names = "Count",
+                 v.names = "count",
                  times=names(test)[2:dim(test)[2]],
                  timevar = "Species",
                  direction="long") #reshape the count table to long table
   test1 <- subset(test1,select = -id) #remove unnecessary id column, which is creating from "reshape" step
-
+  test1$count <- as.numeric(test1$count)
   ## before clean this column, it would be better to remove any space between letters
   test1$Spp <- gsub(" ", "", test1$Spp)
   test2 <- separate(test1,
@@ -78,28 +91,22 @@ CreaCounTable <- function(plotdata_raw){
                               # have a space between letters by accident
                               # I would suggest using "("
 
-  ## this is assigning, rather than removing ()
-  # test2$Status <- rep(c("T","W","F"),dim(test2)[1]/3) #remove brackets in "Status" column
-
   ## after using "(" to seperate, all we need to do is to remove ")"
   test2$Status <- gsub("\\)", "", test2$Status)
 
-  test3 <- cbind(openingid = plotdata_raw[1,3],
-                 date = plotdata_raw[1,11],
-                 plotid = plotdata_raw[1,9],
+  test3 <- cbind(opening = indiplotdata[1,3],
+                 plotid = indiplotdata[1,9],
                  test2)
   test4 <- test3[!apply(is.na(test3),1,any),] #remove rows that contain na
   row.names(test4)<-NULL
   return(test4)
 }
 
-## do the cleanup for the below functions
-
 ####2.FUNCTION for creating ht-age table################
 
-CreaHATable<-function(plotdata_raw){
+CreaHATable<-function(indiplotdata){
 
-  test<-plotdata_raw[12:18,1:8] #extract the ht-age table from the measure table
+  test<-indiplotdata[12:18,1:8] #extract the ht-age table from the measure table
   names(test)<-as.character(test[1,])
   test<-test[-1,]
   test<-test[,!apply(is.na(test),2,all)] #remove na columns
@@ -119,23 +126,24 @@ CreaHATable<-function(plotdata_raw){
     test2<-merge(sub_Ht,sub_Age,by = c("Layer","time"),suffixes = c("Ht","Age"))
     test3<-subset(test2,select = c(Layer,time,numberHt,numberAge))
     names(test3)<-c("Layer","SPP","Ht","Age")
-    test4<-test3[!apply(is.na(test3),1,any),]
-    openingid<-plotdata_raw[1,3] #add opening id
-    plotid<-plotdata_raw[1,9] #add plot id
-    date<-plotdata_raw[1,11] #add measure date
-    test5<-cbind(openingid,date,plotid,test4)
+    test3<-test3[!apply(is.na(test3),1,any),]
+    test4<-cbind(opening = indiplotdata[1,3],
+                 plotid = indiplotdata[1,9],
+                 test3)
+    test4$Ht<-as.numeric(test4$Ht)
+    test4$Age<-as.numeric(test4$Age)
   }else{
-    test5<-NULL
+    test4<-NULL
   }
-  row.names(test5)<-NULL
-  return(test5)
+  row.names(test4)<-NULL
+  return(test4)
 }
 
 ####3.FUNCTION for creating BAF count table#########################
 
-CreaBafTable<-function(plotdata_raw){
+CreaBafTable<-function(indiplotdata){
 
-  test<-plotdata_raw[19:20,1:8]
+  test<-indiplotdata[19:20,1:8]
   names(test)<-as.character(test[1,])
   names(test)[1]<-"BAF"
   test<-test[-1,]
@@ -154,10 +162,10 @@ CreaBafTable<-function(plotdata_raw){
                     fill = "left")
     test2<-subset(test2,select = -id)
     row.names(test2)<-NULL
-    openingid<-plotdata_raw[1,3] #add opening id
-    plotid<-plotdata_raw[1,9] #add plot id
-    date<-plotdata_raw[1,11] #add measure date
-    test3<-cbind(openingid,date,plotid,test2)
+    test3<-cbind(opening = indiplotdata[1,3],
+                 plotid = indiplotdata[1,9],
+                 test2)
+    test3$count<-as.numeric(test3$count)
   }else{
     test3<-NULL
   }
@@ -167,8 +175,9 @@ CreaBafTable<-function(plotdata_raw){
 
 ####4.FUNCTION for Forest Health table###########################
 
-CreaHealTable<-function(fftfile){
-  test<-tmp[3:26,9:12]
+CreaHealTable<-function(indiplotdata){
+
+  test<-indiplotdata[3:26,9:12]
   names(test)<-as.character(test[1,])
   test<-test[-1,]
   test<-test[!apply(is.na(test),1,all),]
@@ -182,10 +191,10 @@ CreaHealTable<-function(fftfile){
     row.names(test1)<-NULL
     test1<-test1[!apply(is.na(test1),1,any),]
     names(test1)[3]<-"Status"
-    openingid<-tmp[1,3] #add opening id
-    plotid<-tmp[1,9] #add plot id
-    date<-tmp[1,11] #add measure date
-    test2<-cbind(openingid,date,plotid,test1)
+    test2<-cbind(opening = indiplotdata[1,3],
+                 plotid = indiplotdata[1,9],
+                 test1)
+    test2$count<-as.numeric(test2$count)
   }else{
     test2<-NULL
   }
@@ -193,7 +202,15 @@ CreaHealTable<-function(fftfile){
   return(test2)
 }
 
+
+
+
+
+
+
+##################################################
 ######TEST RUN for one opening and one plot#######
+##################################################
 FFT_93G_045_568<-read.xlsx(file.path(fftdatapath, "93G_045_568_MPB_Recce_Survey_Strat2.xlsx"))
 for (i in 1:FFT_93G_045_568[9,5]){
   i<-1
