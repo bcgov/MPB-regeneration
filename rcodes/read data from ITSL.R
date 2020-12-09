ca77aed7254dd6a8d92a145de6df9c68e2d59dbf
Chen <Yingbing.Chen@gov.bc.ca>
2020-12-09 15:08:42 -0800

    read ITSL data

---
+++ rcodes/read data from ITSL.R
@@ -0,0 +1,293 @@
+#######################
+##read data from ITSL##
+#######################
+
+rm(list=ls())
+library(data.table)
+library(openxlsx)
+library(sp)
+library(rgdal)
+library(tidyr)
+library(dplyr)
+options(stringsAsFactors = FALSE)
+
+datapath <- "\\\\orbital\\s63016\\!Workgrp\\Inventory\\MPB regeneration_WenliGrp\\raw data"
+datapath_compiled <- "\\\\orbital\\s63016\\!Workgrp\\Inventory\\MPB regeneration_WenliGrp\\compiled data"
+
+ITSLdatapath <- file.path(datapath, "ITSL")
+ITSLdatapath_compiled<-file.path(datapath_compiled,"ITSL")
+
+ITSLdata <- data.table(read.xlsx(file.path(ITSLdatapath, "BCTS ITSL summary TOR 2020-12-04ch.xlsx"),
+                                 sheet = "Scrape",
+                                 detectDates = TRUE))
+ITSLdata <- ITSLdata[,.(id = 1:dim(ITSLdata)[1],
+                        SurveyDate = Survey.date,
+                        TSA,
+                        Location,
+                        Lat = `Latitude.(DD)`,
+                        Long = `Longitude.(DD)`,
+                        Area = Survey.Area,
+                        NPlots = Plots,
+                        BEC,
+                        PliSI = Pli.SI,
+                        PCTnonPba = `%.Estimated.non.pine.conifers.by.basal.area`,
+                        PCTPliKillba = `%.Pli.attack.by.basal.area`,
+                        BAF,
+                        NPlidead = `#.Pli.dead`,
+                        nonPliba = BA.non.Pli,
+                        DeadPliba = BA.Pli.Dead,
+                        LivePliba = BA.Pli.Live,
+                        L1TT,
+                        L2TT,
+                        L3TT,
+                        L4TT,
+                        L1sp1 = INVL1Sp1,
+                        L1sp1pct = `INVL1Sp1%`,
+                        L1sp1age = INVL1Sp1Age,
+                        L1sp1ht = INVL1sp1ht,
+                        L1sp2 = INVL1Sp2,
+                        L1sp2pct = `INVL1Sp2%`,
+                        L1sp2age = INVL1Sp2Age,
+                        L1sp2ht = INVL1Sp2ht,
+                        L1sp3 = INVL1Sp3,
+                        L1sp3pct = `INVL1Sp3%`,
+                        L1sp3age = INVL1Sp3Age,
+                        L1sp3ht = INVL1Sp3ht,
+                        L1sp4 = INVL1Sp4,
+                        L1sp4pct = `INVL1Sp4%`,
+                        L1sp4age = INVL1Sp4Age,
+                        L1sp4ht = INVL1Sp4ht,
+                        L2sp1 = INVL2Sp1,
+                        L2sp1pct = `INVL2Sp1%`,
+                        L2sp1age = INVL2Sp1Age,
+                        L2sp1ht = INVL2sp1ht,
+                        L2sp2 = INVL2Sp2,
+                        L2sp2pct = `INVL2Sp2%`,
+                        L2sp2age = INVL2Sp2Age,
+                        L2sp2ht = INVL2Sp2ht,
+                        L2sp3 = INVL2Sp3,
+                        L2sp3pct = `INVL2Sp3%`,
+                        L2sp3age = INVL2Sp3Age,
+                        L2sp3ht = INVL2Sp3ht,
+                        L2sp4 = INVL2Sp4,
+                        L2sp4pct = `INVL2Sp4%`,
+                        L2sp4age = INVL2Sp4Age,
+                        L2sp4ht = INVL2Sp4ht,
+                        L3shp1 = INVL3Sp1,
+                        L3sp1pct = `INVL3Sp1%`,
+                        L3sp1age = INVL3Sp1Age,
+                        L3sp1ht = INVL3sp1ht,
+                        L3sp2 = INVL3Sp2,
+                        L3sp2pct = `INVL3Sp2%`,
+                        L3sp2age = INVL3Sp2Age,
+                        L3sp3ht = INVL3Sp2ht,
+                        L3sp3 = INVL3Sp3,
+                        L3sp3pct = `INVL3Sp3%`,
+                        L3sp3age = INVL3Sp3Age,
+                        L3sp3ht = INVL3Sp3ht,
+                        L3sp4 = INVL3Sp4,
+                        L3sp4pct = `INVL3Sp4%`,
+                        L3sp4age = INVL3Sp4Age,
+                        L3sp4ht = INVL3Sp4ht,
+                        L4sp1 = INVL4Sp1,
+                        L4sp1pct = `INVL4Sp1%`,
+                        L4sp1age = INVL4Sp1Age,
+                        L4sp1ht = INVL4sp1ht,
+                        L4sp2 = INVL4Sp2,
+                        L4sp2pct = `INVL4Sp2%`,
+                        L4sp2age = INVL4Sp2Age,
+                        L4sp2ht = INVL4Sp2ht,
+                        L4sp3 = INVL4Sp3,
+                        L4sp3pct = `INVL4Sp3%`,
+                        L4sp3age = INVL4Sp3Age,
+                        L4sp3ht = INVL4Sp3ht,
+                        L4sp4 = INVL4Sp4,
+                        L4sp4pct = `INVL4Sp4%`,
+                        L4sp4age = INVL4Sp4Age,
+                        L4sp4ht = INVL4Sp4ht,
+                        L1CC = `INVL1CC%`,
+                        L2CC = `INVL2CC%`,
+                        L3CC = `INVL3CC%`,
+                        L4CC = `INVL4CC%`)]
+
+###convert gps lat long to bcalbers
+
+is.element(TRUE, is.na(ITSLdata[,.(Lat)]))
+is.element(TRUE, is.na(ITSLdata[,.(Long)]))
+
+pointmap <- SpatialPointsDataFrame(ITSLdata[,.(Long, Lat)],
+                                   data = ITSLdata,
+                                   proj4string = CRS("+proj=longlat"))
+
+pointmap_convert <- spTransform(pointmap,
+                                "+proj=aea +lat_1=50
+                                 +lat_2=58.5 +lat_0=45
+                                 +lon_0=-126 +x_0=1000000
+                                 +y_0=0 +ellps=GRS80
+                                 +towgs84=0,0,0,0,0,0,0
+                                 +units=m +no_defs")
+
+bcalbers <- data.frame(pointmap_convert@coords)
+setnames(ITSLdata,"Lat","GPSlat")
+setnames(ITSLdata,"Long","GPSlong")
+ITSLdata <- cbind(ITSLdata, bcalbers)
+
+###Wide table to long table
+
+###1. total tallied numbers and crown closures by layers
+
+# The comment code below use sperate(), the uncomment code use melt()
+# ITSLdata_layer_tt <- ITSLdata[,.(id,
+#                                  L1TT,
+#                                  L2TT,
+#                                  L3TT,
+#                                  L4TT)]
+# ITSLdata_layer_tt <- reshape(data = ITSLdata_layer_tt,
+#                              varying = c("L1TT", "L2TT", "L3TT", "L4TT"),
+#                              v.names = "TT",
+#                              timevar = "Layer",
+#                              idvar = "id",
+#                              direction = "long")
+#
+# ITSLdata_layer_cc <- ITSLdata[,.(id,
+#                                  L1CC,
+#                                  L2CC,
+#                                  L3CC,
+#                                  L4CC)]
+# ITSLdata_layer_cc <- reshape(data = ITSLdata_layer_cc,
+#                              varying = c("L1CC", "L2CC", "L3CC", "L4CC"),
+#                              v.names = "CC",
+#                              timevar = "Layer",
+#                              idvar = "id",
+#                              direction ="long")
+# ITSLdata_layer <- merge(ITSLdata_layer_cc, ITSLdata_layer_tt, by = c("id", "Layer"))
+
+ITSLdata_layer <- ITSLdata[,.(id,
+                              L1TT,
+                              L2TT,
+                              L3TT,
+                              L4TT,
+                              L1CC,
+                              L2CC,
+                              L3CC,
+                              L4CC)]
+ITSLdata_layer <- melt(data = test,
+                       id.vars = "id",
+                       measure.vars = list(c("L1TT", "L2TT", "L3TT", "L4TT"), c("L1CC", "L2CC", "L3CC", "L4CC")),
+                       variable.name = "Layer",
+                       value.name = c("TT", "CC"),
+                       variable.factor = FALSE)
+
+
+
+####2. species, age, ht by layers
+
+ITSL_layer_sp <- ITSLdata[,.(id,
+                             L1sp1,
+                             L1sp1pct,
+                             L1sp1age,
+                             L1sp1ht,
+                             L1sp2,
+                             L1sp2pct,
+                             L1sp2age,
+                             L1sp2ht,
+                             L1sp3,
+                             L1sp3pct,
+                             L1sp3age,
+                             L1sp3ht,
+                             L1sp4,
+                             L1sp4pct,
+                             L1sp4age,
+                             L1sp4ht,
+                             L2sp1,
+                             L2sp1pct,
+                             L2sp1age,
+                             L2sp1ht,
+                             L2sp2,
+                             L2sp2pct,
+                             L2sp2age,
+                             L2sp2ht,
+                             L2sp3,
+                             L2sp3pct,
+                             L2sp3age,
+                             L2sp3ht,
+                             L2sp4,
+                             L2sp4pct,
+                             L2sp4age,
+                             L2sp4ht,
+                             L3shp1,
+                             L3sp1pct,
+                             L3sp1age,
+                             L3sp1ht,
+                             L3sp2,
+                             L3sp2pct,
+                             L3sp2age,
+                             L3sp3ht,
+                             L3sp3,
+                             L3sp3pct,
+                             L3sp3age,
+                             L3sp3ht,
+                             L3sp4,
+                             L3sp4pct,
+                             L3sp4age,
+                             L3sp4ht,
+                             L4sp1,
+                             L4sp1pct,
+                             L4sp1age,
+                             L4sp1ht,
+                             L4sp2,
+                             L4sp2pct,
+                             L4sp2age,
+                             L4sp2ht,
+                             L4sp3,
+                             L4sp3pct,
+                             L4sp3age,
+                             L4sp3ht,
+                             L4sp4,
+                             L4sp4pct,
+                             L4sp4age,
+                             L4sp4ht)]
+
+L1 <- ITSL_layer_sp[,c(1,grep("L1", names(ITSL_layer_sp))), with = FALSE]
+L1_sp <- NULL
+for ( i in unique(L1$id)){
+  sp1 <- L1[id %in% i, .(id = i, Layer = "1", SP = L1sp1, PCT = L1sp1pct, AGE = L1sp1age, HT = L1sp1ht)]
+  sp2 <- L1[id %in% i, .(id = i, Layer = "1", SP = L1sp2, PCT = L1sp2pct, AGE = L1sp2age, HT = L1sp2ht)]
+  sp3 <- L1[id %in% i, .(id = i, Layer = "1", SP = L1sp3, PCT = L1sp3pct, AGE = L1sp3age, HT = L1sp3ht)]
+  sp4 <- L1[id %in% i, .(id = i, Layer = "1", SP = L1sp4, PCT = L1sp4pct, AGE = L1sp4age, HT = L1sp4ht)]
+  sp <- rbind(sp1,sp2,sp3,sp4)
+  L1_sp <- rbind(L1_sp, sp)
+}
+L2 <- ITSL_layer_sp[,c(1,grep("L2", names(ITSL_layer_sp))), with = FALSE]
+L2_sp <- NULL
+for ( i in unique(L2$id)){
+  sp1 <- L2[id %in% i, .(id = i, Layer = "2", SP = L2sp1, PCT = L2sp1pct, AGE = L2sp1age, HT = L2sp1ht)]
+  sp2 <- L2[id %in% i, .(id = i, Layer = "2", SP = L2sp2, PCT = L2sp2pct, AGE = L2sp2age, HT = L2sp2ht)]
+  sp3 <- L2[id %in% i, .(id = i, Layer = "2", SP = L2sp3, PCT = L2sp3pct, AGE = L2sp3age, HT = L2sp3ht)]
+  sp4 <- L2[id %in% i, .(id = i, Layer = "2", SP = L2sp4, PCT = L2sp4pct, AGE = L2sp4age, HT = L2sp4ht)]
+  sp <- rbind(sp1,sp2,sp3,sp4)
+  L2_sp <- rbind(L2_sp, sp)
+}
+L3 <- ITSL_layer_sp[,c(1,grep("L3", names(ITSL_layer_sp))), with = FALSE]
+L3_sp <- NULL
+for ( i in unique(L3$id)){
+  sp1 <- L3[id %in% i, .(id = i, Layer = "3", SP = L3sp1, PCT = L3sp1pct, AGE = L3sp1age, HT = L3sp1ht)]
+  sp2 <- L3[id %in% i, .(id = i, layer = "3", SP = L3sp2, PCT = L3sp2pct, AGE = L3sp2age, HT = L3sp2ht)]
+  sp3 <- L3[id %in% i, .(id = i, Layer = "3", SP = L3sp3, PCT = L3sp3pct, AGE = L3sp3age, HT = L3sp3ht)]
+  sp4 <- L3[id %in% i, .(id = i, Layer = "3", SP = L3sp4, PCT = L3sp4pct, AGE = L3sp4age, HT = L3sp4ht)]
+  sp <- rbind(sp1,sp2,sp3,sp4)
+  L3_sp <- rbind(L3_sp, sp)
+}
+L4 <- ITSL_layer_sp[,c(1,grep("L4", names(ITSL_layer_sp))), with = FALSE]
+L4_sp <- NULL
+for ( i in unique(L4$id)){
+  sp1 <- L4[id %in% i, .(id = i, Layer = "4", SP = L4sp1, PCT = L4sp1pct, AGE = L4sp1age, HT = L4sp1ht)]
+  sp2 <- L4[id %in% i, .(id = i, layer = "4", SP = L4sp2, PCT = L4sp2pct, AGE = L4sp2age, HT = L4sp2ht)]
+  sp3 <- L4[id %in% i, .(id = i, Layer = "4", SP = L4sp3, PCT = L4sp3pct, AGE = L4sp3age, HT = L4sp3ht)]
+  sp4 <- L4[id %in% i, .(id = i, Layer = "4", SP = L4sp4, PCT = L4sp4pct, AGE = L4sp4age, HT = L4sp4ht)]
+  sp <- rbind(sp1,sp2,sp3,sp4)
+  L4_sp <- rbind(L4_sp, sp)
+}
+
+ITSL_sp <- rbind(L1_sp, L2_sp, L3_sp, L4_sp)
+
