143641868eb0b1bbd2a1e66655d63754819d6878
Chen <Yingbing.Chen@gov.bc.ca>
2021-05-18 10:14:54 -0700

    1. Add SP0 for all plots 2. for ITSL, use beczones from ITSL to replace 2019's and 2003's beczones

---
+++ rcodes/3.2. combined data cleaning.R
@@ -0,0 +1,82 @@
+rm(list=ls())
+library(data.table)
+library(tidyr)
+library(reshape2)
+library(dplyr)
+
+invlayer <- data.table(read.csv("J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Combined/combined_layer.csv"))
+invpoly <- data.table(read.csv("J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Combined/combined_poly.csv"))
+
+#Add SP0 for all plots?
+
+unique(invlayer$SP)
+
+for (i in 1:dim(invlayer)[1]){
+  if(invlayer[i, SP] == "PL"){
+    invlayer[i, SP0 := "PL"]
+  }
+  if(invlayer[i, SP] == "SB"|invlayer[i, SP] == "SX"|invlayer[i, SP] == "SW"|invlayer[i, SP] == "S"|invlayer[i, SP] == "SE"){
+    invlayer[i, SP0 := "S"]
+  }
+  if(invlayer[i, SP] == "AT"){
+    invlayer[i, SP0 := "AT"]
+  }
+  if(invlayer[i, SP] == "EP" | invlayer[i, SP] == "E"){
+    invlayer[i, SP0 := "E"]
+  }
+  if(invlayer[i, SP] == "BL" | invlayer[i, SP] == "B"){
+    invlayer[i, SP0 := "B"]
+  }
+  if(invlayer[i, SP] == "FD"){
+    invlayer[i, SP0 := "F"]
+  }
+  if(invlayer[i, SP] == "AC"){
+    invlayer[i, SP0 := "AC"]
+  }
+  if(invlayer[i, SP] == "CW"){
+    invlayer[i, SP0 := "C"]
+  }
+  if(invlayer[i, SP] == "PY"){
+    invlayer[i, SP0 := "PY"]
+  }
+  if(invlayer[i, SP] == "PA"){
+    invlayer[i, SP0 := "PA"]
+  }
+  if(invlayer[i, SP] == "H" | invlayer[i, SP] == "HW"){
+    invlayer[i, SP0 := "H"]
+  }
+  if(invlayer[i, SP] == "LW"){
+    invlayer[i, SP0 := "L"]
+  }
+  if(invlayer[i, SP] == "PW"){
+    invlayer[i, SP0 := "PW"]
+  }
+  if(invlayer[i, SP] == "UNK"){
+    invlayer[i, SP0 := "UNK"]
+  }
+}
+
+
+##bec zones in ITSL's survey can be different from VRI2019's
+##unify ITSL survey's bec zone
+##use bec zone from ITSL to replace 2019's and 2003's
+
+bec <- distinct(invlayer[Data_Source %in% "ITSL" & Layer %in% c("L1/L2", "L3/L4"), .(id, BEC, subBEC, vaBEC, BEC_sub_va)])
+lid <- invlayer[Data_Source %in% "ITSL", .(id)]
+lbec <- merge(lid, bec, by ="id", all.x = TRUE)
+invlayer[Data_Source %in% "ITSL", ':=' (BEC = lbec$BEC,
+                                        subBEC = lbec$subBEC,
+                                        vaBEC = lbec$vaBEC,
+                                        BEC_sub_va = lbec$BEC_sub_va)]
+
+pid <- invpoly[Data_Source %in% "ITSL", .(id)]
+pbec <- merge(pid, bec, by = "id", all.x = TRUE)
+setorder(invpoly, id)
+setorder(pbec,id)
+invpoly[Data_Source %in% "ITSL", ':=' (BEC = pbec$BEC,
+                                       subBEC = pbec$subBEC,
+                                       vaBEC = pbec$vaBEC,
+                                       BEC_sub_va = pbec$BEC_sub_va)]
+
+write.csv(invlayer, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Combined/combined_layer.csv", row.names = FALSE, na = "")
+write.csv(invpoly, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Combined/combined_poly.csv", row.names = FALSE, na = "")
