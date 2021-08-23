e2086f491ff9508f5f992ebb3023aa9b09ce41bf
Chen <Yingbing.Chen@gov.bc.ca>
2021-08-23 13:35:22 -0700

    correct wrong code that removes overstory crown closure

--- rcodes/4.2 (cont2) understory data cleaning_species check and name unify.R
+++ rcodes/4.2 (cont2) understory data cleaning_species check and name unify.R
@@ -9,10 +9,10 @@ unique(under1$SP)
 #[1] "PLI" "BL"  "SX"  "PA"  "SE"  "FDI" "PY"  "HW"  "BA"  "PW"
 #[11] "JR"
 
-under1[SP %in% "PLI", SP := "PL"]
-under1[SP %in% "FDI", SP := "FD"]
+under[SP %in% "PLI", SP := "PL"]
+under[SP %in% "FDI", SP := "FD"]
 
-under1[SP %in% "JR"]
-under1[Plot %in% "9010U_3PT"]
+under[SP %in% "JR"]
+under[Plot %in% "9010U_3PT"]
 
-write.csv(under1, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Lillooet/understory_cleaned.csv", row.names = FALSE, na = "")
+write.csv(under, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Lillooet/understory_cleaned.csv", row.names = FALSE, na = "")
