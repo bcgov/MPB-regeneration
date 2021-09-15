a887645554186546194e075c89004475ab6c52ac
Chen <Yingbing.Chen@gov.bc.ca>
2021-09-15 09:55:33 -0700

    change the class of regen to "R" instead of extract regen, keep the information of the other class

--- rcodes/4.4 Extract regen from understory.R
+++ rcodes/4.4 Extract regen from understory.R
@@ -13,27 +13,31 @@ newunder <- merge(under, dist, by = "Plot")
 newunder[,interval := Survey_Date-Dist_year]
 newunder[,max := interval+5]
 
-regen <- newunder[Age < max]
+newunder$Class <- as.character(newunder$Class)
+newunder[Age < max, Class := "R"]
+newunder[Class %in% 2, Class := "U2"]
+newunder[Class %in% 3, Class := "U3"]
+newunder[Class %in% 1, Class := "U1"]
 
 ####3 points plot
 ####calculate avearge count, DBH, age, ht for each plot
 
-regen[FHQ %in% "H", Count := Count*2]
-regen[FHQ %in% "Q", Count := Count*4]
-regen[,FHQ := NULL]
+newunder[FHQ %in% "H", Count := Count*2]
+newunder[FHQ %in% "Q", Count := Count*4]
+newunder[,FHQ := NULL]
 
-regen1 <- regen[,.(Count = round(sum(Count, na.rm = TRUE)/3, digits = 2),
-                   DBH = round(mean(DBH, na.rm=TRUE), digits = 2),
-                   Age = round(mean(Age, na.rm = TRUE), digits = 0),
-                   Ht = round(mean(Ht, na.rm = TRUE), digits = 2),
-                   OVER_CC = round(mean(OVER_CC), digits = 0),
-                   BEC = unique(BEC),
-                   subBEC = unique(subBEC),
-                   vaBEC = unique(vaBEC),
-                   Dist_year = unique(Dist_year),
-                   Survey_Date = unique(Survey_Date),
-                   interval = unique(interval)),
-                by = .(Plot, Class, SP)]
-regen1[DBH %in% NaN, DBH := NA]
+newunder1 <- newunder[,.(Count = round(sum(Count, na.rm = TRUE)/3, digits = 2),
+                         DBH = round(mean(DBH, na.rm=TRUE), digits = 2),
+                         Age = round(mean(Age, na.rm = TRUE), digits = 0),
+                         Ht = round(mean(Ht, na.rm = TRUE), digits = 2),
+                         OVER_CC = round(mean(OVER_CC), digits = 0),
+                         BEC = unique(BEC),
+                         subBEC = unique(subBEC),
+                         vaBEC = unique(vaBEC),
+                         Dist_year = unique(Dist_year),
+                         Survey_Date = unique(Survey_Date),
+                         interval = unique(interval)),
+                      by = .(Plot, Class, SP)]
+newunder1[DBH %in% NaN, DBH := NA]
 
-write.csv(regen1, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Lillooet/understory_cleaned_SI_Cl1Updated_regen.csv", row.names = FALSE)
+write.csv(newunder1, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/Lillooet/understory_cleaned_SI_Cl1Updated_regen.csv", row.names = FALSE)
