e00a1fb56a821279cc146363c2e93cb26967ea98
Chen <Yingbing.Chen@gov.bc.ca>
2021-11-29 10:36:43 -0800

    calculate speceis pct for L1/L2 and L3/L4 layers as well Remove sumCount column

--- rcodes/1.8 Erafor_Regen defined.R
+++ rcodes/1.8 Erafor_Regen defined.R
@@ -1,9 +1,5 @@
 rm(list=ls())
 library(data.table)
-library(ggplot2)
-library(cowplot)
-library(randomForest)
-
 
 invlayer <- data.table(read.csv("J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/From Erafor/Erafor_layer_cleaned.csv"))
 invpoly <- data.table(read.csv("J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/From Erafor/Erafor_poly_cleaned_addistdate.csv"))
@@ -18,8 +14,9 @@ regen <- invlayer2[Layer %in% "L3/L4" & Age < max]
 invlayer2[Layer %in% "L3/L4" & Age < max, Layer := "R"]
 
 
-invlayer2[Layer %in% "R", sumCount := sum(Count), by = id]
-invlayer2[Layer %in% "R", PCT := round(100*Count/sumCount, digits = 0)]
-invlayer2[Layer %in% "R"]
+invlayer2[Layer %in% c("R", "L1/L2", "L3/L4")]
+invlayer2[Layer %in% c("R", "L1/L2", "L3/L4"), sumCount := sum(Count), by = .(id, Layer)]
+invlayer2[Layer %in% c("R", "L1/L2", "L3/L4"), PCT := round(100*Count/sumCount, digits = 0)]
+invlayer2[,sumCount := NULL]
 
 write.csv(invlayer2, "J:/!Workgrp/Inventory/MPB regeneration_WenliGrp/compiled data/From Erafor/Erafor_layer_cleaned_regendefined.csv", row.names = FALSE)
